a61e4e7493996704b15c8689b4e630e63c8829b9
muhammedzahid123 <zaaahid2109@gmail.com>
2024-11-24 17:22:39 +0400

    Add files via upload

---
+++ #Exercise 9.py
@@ -0,0 +1,13 @@
+#Exercise 9
+
+# Define a function to print a greeting
+def hello():
+    print("Hello")
+
+# Main function to execute program logic
+def main():
+    hello()  # Call the hello function
+
+# Entry point of the script
+if __name__ == "_main_":
+    main()
