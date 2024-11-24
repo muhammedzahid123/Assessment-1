5577722d677897e34ad7d0e83cae1872b7058209
muhammedzahid123 <zaaahid2109@gmail.com>
2024-11-24 17:19:49 +0400

    Add files via upload

---
+++ #Exercise 1 Coding is cool.py
@@ -0,0 +1,10 @@
+#Exercise 1 Coding is cool
+
+# Define variables
+Variable1 = "Coding"
+Variable2 = "is"
+variable3 = "Cool"
+# To form the phrase, you have to use string concatenation.
+phrase = Variable1 + " " + Variable2 + " " + variable3
+# Print out the following phrase to the console
+print(phrase)#
