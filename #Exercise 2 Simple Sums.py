a5b2696cbc8333d26d04a65abadad996ab0cb21b
muhammedzahid123 <zaaahid2109@gmail.com>
2024-11-24 17:20:27 +0400

    Add files via upload

---
+++ #Exercise 2 Simple Sums.py
@@ -0,0 +1,9 @@
+#Exercise 2 Simple Sums
+# Define the first number
+number1 = 8
+# Define the second number
+number2 = 10
+# Compute the sum of the two numbers
+sum_result = number1 + number2
+# Display the result to the user
+print(f"The sum of {number1} and {number2} is: {sum_result}")
