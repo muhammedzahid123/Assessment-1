41f6dd18a1c4373f2e0d1b7bef800325f0b5d5d8
muhammedzahid123 <zaaahid2109@gmail.com>
2024-11-24 17:22:01 +0400

    Add files via upload

---
+++ #Exercise 7 some Counting.py
@@ -0,0 +1,30 @@
+#Exercise 7 some Counting
+
+# Count up from 0 to 50
+print("Counting from 0 to 50:")
+for i in range(51):
+    print(i)
+print("-" * 40)
+
+# Count down from 50 to 0
+print("Counting from 50 to 0:")
+for i in range(50, -1, -1):
+    print(i)
+print("-" * 40)
+
+# Count up from 30 to 50
+print("Counting from 30 to 50:")
+for i in range(30, 51):
+    print(i)
+print("-" * 40)
+
+# Count down from 50 to 10, steps of 2
+print("Counting from 50 to 10 by 2s:")
+for i in range(50, 9, -2):
+    print(i)
+print("-" * 40)
+
+# Count up from 100 to 200, steps of 5
+print("Counting from 100 to 200 by 5s:")
+for i in range(100, 201, 5):
+    print(i)
